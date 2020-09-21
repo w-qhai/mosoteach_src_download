499506643dd23d640e5228210b68830ae7b6d401
QwQhai <w_qhai@yeah.net>
2020-09-21 10:42:13 +0800

    支持分组后的资源下载

--- content.js
+++ content.js
@@ -1,13 +1,15 @@
-var hide_div = document.getElementsByClassName("hide-div")[0]
-var resource = hide_div.children
-
+var hide_divs = document.getElementsByClassName("hide-div")
 var src_url = []
-for (var i = 0; i < resource.length; i++) {
-    src_url.push(resource[i].dataset.href)
-    var ul = resource[i].getElementsByTagName("ul")[0];
-    var a_tag = document.createElement('a');
-    a_tag.innerHTML = "点击下载"
-    a_tag.setAttribute("href", resource[i].dataset.href)
-    ul.appendChild(a_tag);
+var hide_div
+for (var j = 0; j < hide_divs.length; j++) {
+    hide_div = hide_divs[j]
+    for (var i = 0; i < hide_div.childElementCount; i++) {
+        src_url.push(hide_div.children[i].dataset.href)
+        var ul = hide_div.children[i].getElementsByTagName("ul")[0];
+        var a_tag = document.createElement('a');
+        a_tag.innerHTML = "点击下载"
+        a_tag.setAttribute("href", hide_div.children[i].dataset.href)
+        ul.appendChild(a_tag);
+    }
 }
 
